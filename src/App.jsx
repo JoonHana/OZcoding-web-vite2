6468ca9f2aaefe5614b7a1100443c27fbbf843bf
JoonHana <c98716637@gmail.com>
2025-12-30 18:14:01 +0900

    18

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import image111 from "../image/image111.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,6 +19,7 @@ function App() {
       </div>
       <h1>김흥욱 어흥욱 끼얏허허이야지잉펑펑펑</h1>
       <h1>긍이애옹 빛이애옹 곰이애옹 콩콩이애옹</h1>
+      <img src={image111} alt="image" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
