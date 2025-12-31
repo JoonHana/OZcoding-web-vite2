513bec5e5005f036ca82ba07d38b9245cb7f65c1
JoonHana <c98716637@gmail.com>
2025-12-31 17:41:44 +0900

    광고5

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import img111 from "./img/img.png";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="https://www.sac.or.kr/site/main/show/show_view?SN=78576"
+          target="_blank"
+        >
+          <img src={img111} alt="리사이틀" />
         </a>
       </div>
       <h3> 문예진 & 최이란 듀오 리사이틀</h3>
       <h4> 2026. 02. 12 (목) 19:30 </h4>
       <h4> 예술의 전당 인춘아트홀 </h4>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
